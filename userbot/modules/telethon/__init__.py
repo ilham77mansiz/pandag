8e63c087045e7680c057abf975e852469295b1b0
Ilham Mansiz <77713850+ilhammansiz@users.noreply.github.com>
2023-01-26 00:24:13 +0700

    Update __init__.py

--- userbot/modules/telethon/__init__.py
+++ userbot/modules/telethon/__init__.py
@@ -17,7 +17,6 @@ from ..._misc.session import PandaBot
 from ...helpers import *
 from ...helpers.utils import _format, _pandatools, _pandautils, install_pip, reply_id
 from telethon import events
-from ...sql_helper.globals import gvarstatus
 
 # =================== CONSTANT ===================
 def mansiez(**args):
