2fc23624414cb656b3ee96fc8d28587ebf396a54
Ilham Mansiz <77713850+ilhammansiz@users.noreply.github.com>
2023-01-25 20:05:04 +0700

    Update client.py

--- userbot/Session/client.py
+++ userbot/Session/client.py
@@ -26,81 +26,83 @@ except Exception as e:
     print(f"STRING_SESSION - {e}")
     sys.exit()
 
-if SESSION2:
-    session2 = StringSession(str(Var.SESSION2))
-else:
-    session2 = "PandaUserBot"
+
 try:
-    bot2 = TelegramClient(
-        session=session2,
-        api_id=Var.API_ID,
-        api_hash=Var.API_HASH,
-        connection=ConnectionTcpAbridged,
-        auto_reconnect=True,
-        connection_retries=None,
-    )
-    call_py2 = PyTgCalls(bot2)
+    if SESSION2:
+        session2 = StringSession(str(Var.SESSION2))
+        bot2 = TelegramClient(
+            session=session2,
+            api_id=Var.API_ID,
+            api_hash=Var.API_HASH,
+            connection=ConnectionTcpAbridged,
+            auto_reconnect=True,
+            connection_retries=None,
+        )
+        call_py2 = PyTgCalls(bot2)
+    else:
+        bot2 = None
+        call_py2 = None
 except Exception as e:
     print(f"STRING_SESSION2 - {e}")
     sys.exit()
 
 
-if SESSION3:
-    session3 = StringSession(str(Var.SESSION3))
-else:
-    session3 = "PandaUserBot"
 try:
-    bot3 = TelegramClient(
-        session=session3,
-        api_id=Var.API_ID,
-        api_hash=Var.API_HASH,
-        connection=ConnectionTcpAbridged,
-        auto_reconnect=True,
-        connection_retries=None,
-    )
-    call_py3 = PyTgCalls(bot3)
+    if SESSION3:
+        session2 = StringSession(str(Var.SESSION3))
+        bot2 = TelegramClient(
+            session=session3,
+            api_id=Var.API_ID,
+            api_hash=Var.API_HASH,
+            connection=ConnectionTcpAbridged,
+            auto_reconnect=True,
+            connection_retries=None,
+        )
+        call_py3 = PyTgCalls(bot3)
+    else:
+        bot3 = None
+        call_py3 = None
 except Exception as e:
     print(f"STRING_SESSION3 - {e}")
     sys.exit()
 
 
-if SESSION4:
-    session4 = StringSession(str(Var.SESSION4))
-else:
-    session4 = "PandaUserBot"
 try:
-    bot4 = TelegramClient(
-        session=session4,
-        api_id=Var.API_ID,
-        api_hash=Var.API_HASH,
-        connection=ConnectionTcpAbridged,
-        auto_reconnect=True,
-        connection_retries=None,
-    )
-    call_py4 = PyTgCalls(bot4)
+    if SESSION4:
+        session2 = StringSession(str(Var.SESSION4))
+        bot2 = TelegramClient(
+            session=session4,
+            api_id=Var.API_ID,
+            api_hash=Var.API_HASH,
+            connection=ConnectionTcpAbridged,
+            auto_reconnect=True,
+            connection_retries=None,
+        )
+        call_py4 = PyTgCalls(bot4)
+    else:
+        bot4 = None
+        call_py4 = None
 except Exception as e:
     print(f"STRING_SESSION4 - {e}")
     sys.exit()
 
 
 
-
-if SESSION5:
-    session5 = StringSession(str(Var.SESSION5))
-else:
-    session5 = "PandaUserBot"
 try:
-    bot5 = TelegramClient(
-        session=session5,
-        api_id=Var.API_ID,
-        api_hash=Var.API_HASH,
-        connection=ConnectionTcpAbridged,
-        auto_reconnect=True,
-        connection_retries=None,
-    )
-    call_py5 = PyTgCalls(bot5)
+    if SESSION5:
+        session2 = StringSession(str(Var.SESSION5))
+        bot2 = TelegramClient(
+            session=session5,
+            api_id=Var.API_ID,
+            api_hash=Var.API_HASH,
+            connection=ConnectionTcpAbridged,
+            auto_reconnect=True,
+            connection_retries=None,
+        )
+        call_py5 = PyTgCalls(bot5)
+    else:
+        bot5 = None
+        call_py5 = None
 except Exception as e:
     print(f"STRING_SESSION5 - {e}")
     sys.exit()
-
-
