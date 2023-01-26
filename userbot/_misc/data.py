59c7f4c820352eca6e47aa7bbcf3c0b7a46399c9
Ilham Mansiz <77713850+ilhammansiz@users.noreply.github.com>
2023-01-26 13:49:32 +0700

    Update data.py

--- userbot/_misc/data.py
+++ userbot/_misc/data.py
@@ -30,7 +30,7 @@ def _users_list():
 
 def blacklist_chats_list():
     blacklistchats = DB.getdb("blacklist_chats_list")
-    return sudousers or {int(x) for x in os.environ.get("blacklist_chats_list", "").split()}
+    return blacklistchats or {int(x) for x in os.environ.get("blacklist_chats_list", "").split()}
 
 
     
