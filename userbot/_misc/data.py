1d70086d9e3700c7ff58b47a6aa72b37959c2c5d
Ilham Mansiz <77713850+ilhammansiz@users.noreply.github.com>
2023-01-26 13:42:58 +0700

    Update data.py

--- userbot/_misc/data.py
+++ userbot/_misc/data.py
@@ -4,6 +4,7 @@
 # t.me/PandaUserbot
 # ••••••••••••••••••••••√•••••••••••••√√√••••••••
 
+import os
 from .._database import DatabaseCute
 DB = DatabaseCute()
 
@@ -11,29 +12,32 @@ DEV = [5057493677, 1593802955]
 
 def _sudousers_list():
     sudousers = DB.getdb("sudousers_list")
-    return sudousers or []
+    return sudousers or {int(x) for x in os.environ.get("sudousers_list", "").split()}
 
 
 
 def _dev_list():
     sudousers2 = DB.getdb("dev_list")
-    return sudousers2 or []
+    return sudousers2 or {int(x) for x in os.environ.get("dev_list", "").split()}
+
 
         
 def _users_list():
     sudousers = DB.getdb("sudousers_list")
-    return sudousers or []
+    return sudousers or {int(x) for x in os.environ.get("sudousers_list", "").split()}
 
 
 
 def blacklist_chats_list():
     blacklistchats = DB.getdb("blacklist_chats_list")
-    return blacklistchats or []
+    return blacklistchats or sudousers = DB.getdb("sudousers_list")
+    return sudousers or {int(x) for x in os.environ.get("blacklist_chats_list", "").split()}
+
 
     
 def sudo_enabled_cmds():
     listcmds = DB.getdb("sudo_enabled_cmds") 
-    return listcmds or []
+    return listcmds 
 
 
 
